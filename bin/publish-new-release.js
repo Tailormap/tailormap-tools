5d35a64647522fce065b48a197f766fb44a8f229
Geert Plaisier <geert.plaisier@gnamic.nl>
2025-01-30 10:19:43 +0100

    Request version for single release

--- bin/publish-new-release.js
+++ bin/publish-new-release.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 'use strict';
 
-const {checkCleanGitRepo, requestLibrary, getCliArgument, hasCliArgument, publishLibrary} = require("./helpers/shared");
+const {checkCleanGitRepo, requestLibrary, getCliArgument, hasCliArgument, publishLibrary, requestVersion} = require("./helpers/shared");
 
 checkCleanGitRepo();
 
-const library = getCliArgument('--library');
-const version = getCliArgument('--version');
-const dryRun = hasCliArgument('--dry-run');
+(async function main() {
+  const library = getCliArgument('--library');
+  let version = getCliArgument('--version');
+  const dryRun = hasCliArgument('--dry-run');
 
-if (library) {
-  try {
-    publishLibrary(library, version, dryRun);
-  } catch (e) {
-    console.log('Error occured', e);
+  if (version === null) {
+    version = await requestVersion()
   }
-} else {
-  requestLibrary('Select the library for which to make a release', lib => publishLibrary(lib, version, dryRun));
-}
+
+  if (!version) {
+    console.error('Supply version');
+    process.exit(1);
+  }
+
+  if (library) {
+    try {
+      publishLibrary(library, version, dryRun);
+    } catch (e) {
+      console.log('Error occured', e);
+    }
+  } else {
+    requestLibrary('Select the library for which to make a release', lib => publishLibrary(lib, version, dryRun));
+  }
+})();
