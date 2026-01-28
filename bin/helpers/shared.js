3cf9346470401f8c5ecdfc6b64ec8ed0636cae9e
Geert Plaisier <geert.plaisier@gnamic.nl>
2026-01-28 10:33:30 +0100

    Added new Link Libraries script

--- bin/helpers/shared.js
+++ bin/helpers/shared.js
@@ -238,6 +238,17 @@ const clearCache = async () => {
   await runCommand('rm', ['-rf', '.nx'], getPathFromProjectRoot());
 }
 
+
+// Colors for terminal output
+const consoleMarkup = {
+  red: (text) => `\x1b[31m${text}\x1b[0m`,
+  green: (text) => `\x1b[32m${text}\x1b[0m`,
+  yellow: (text) => `\x1b[33m${text}\x1b[0m`,
+  blue: (text) => `\x1b[34m${text}\x1b[0m`,
+  bold: (text) => `\x1b[1m${text}\x1b[0m`,
+  boldGreen: (text) => `\x1b[1m\x1b[32m${text}\x1b[0m`,
+};
+
 export {
   requestLibrary,
   requestVersion,
@@ -250,5 +261,6 @@ export {
   sleep,
   clearCache,
   getPathFromProjectRoot,
-  getTailormapProjectFile
+  getTailormapProjectFile,
+  consoleMarkup,
 };
